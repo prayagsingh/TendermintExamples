f10589456c73a49998aee51bbd553cb4a1b8c5ce
Prayag Singh <prayagsingh@users.noreply.github.com>
2019-02-13 15:46:39 +0530

    Fixed Check_tx bug,  added support to add the validators via RPC
    1. Fixed Check_Tx bug. Now checking transaction and height counter
    2. added support to add the validators via RPC.

--- CustomExample/OurApp/Operations.go
+++ CustomExample/OurApp/Operations.go
@@ -5,7 +5,9 @@ import (
 	"time"
 
 	//"reflect"
+	b64 "encoding/base64"
 	"encoding/binary"
+	"encoding/hex"
 	"encoding/json"
 	"strconv"
 	"strings"
@@ -20,6 +22,11 @@ import (
 var _ types.Application = (*OperationApplication)(nil)
 var db *mgo.Database
 
+// Prefix for validators
+const (
+	ValidatorSetChangePrefix string = "val:"
+)
+
 // StoreResult structure to store the data to the mongoDb
 type StoreResult struct {
 	TxnID     bson.ObjectId `bson:"_id"       json:"_id"`
@@ -30,12 +37,19 @@ type StoreResult struct {
 	Timestamp time.Time     `bson:"timestamp" json:"timestamp"`
 }
 
+// MyValidator Struct ---> using it to insert the ValidatorUpdate result to monooDB
+type MyValidator struct {
+	Name   string `bson:"name" json:"name"`
+	PubKey []byte `bson:"pubkeys" json:"pubkeys"`
+	Power  int64  `bson:"power" json:"power"`
+}
+
 // MyState using State structure
 type MyState struct {
-	state   sm.State
-	TotTxn  int64  `bson:"tottxn"`
-	AppHash []byte `bson:"app_hash"`
-	Height  int64  `bson:"height"`
+	InfoState sm.State
+	TotTxn    int64  `bson:"tottxn"`
+	AppHash   []byte `bson:"app_hash"`
+	Height    int64  `bson:"height"`
 }
 
 // function to save the current state so that it can be loaded from the same state
@@ -94,7 +108,11 @@ type OperationApplication struct {
 	types.BaseApplication
 	//txnCount int // stores the count of all the total deliver_tx
 	//hashCout int // stores the count of total commit
-	state MyState
+	State MyState
+	// validator updates
+	Validator MyValidator
+	//validator set
+	ValUpdates []types.ValidatorUpdate
 }
 
 // NewOperationApplication which returns OperationApplication struct
@@ -102,7 +120,7 @@ func NewOperationApplication(dbCopy *mgo.Database) *OperationApplication {
 	db = dbCopy
 	fmt.Print("\n Inside NewOperation function")
 	state := loadState(db)
-	return &OperationApplication{state: state}
+	return &OperationApplication{State: state}
 }
 
 // Tendermint methods
@@ -114,21 +132,24 @@ func (app *OperationApplication) Info(req types.RequestInfo) (resInfo types.Resp
 		"\n Block Version: ", req.BlockVersion,
 		"\n P2P version: ", req.P2PVersion, "\n")
 
-	resInfo.LastBlockHeight = app.state.Height
-	resInfo.LastBlockAppHash = app.state.AppHash
+	// Usin below for testing purposes else value is already assigned in return statement
+	resInfo.LastBlockHeight = app.State.Height
+	resInfo.LastBlockAppHash = app.State.AppHash
 	fmt.Print("\n Value of Last Block Height: ", resInfo.LastBlockHeight,
 		"\n Last block App hash: ", fmt.Sprintf("%x", resInfo.LastBlockAppHash))
 	//return resInfo
 	return types.ResponseInfo{
-		Data:             fmt.Sprintf("{\"Total txns so far\":%v}", app.state.TotTxn),
+		Data:             fmt.Sprintf("{\"Total txns so far\":%v}", app.State.TotTxn),
 		Version:          req.Version,
-		LastBlockHeight:  app.state.Height,
-		LastBlockAppHash: app.state.AppHash}
+		LastBlockHeight:  app.State.Height,
+		LastBlockAppHash: app.State.AppHash}
 }
 
 // BeginBlock   ---> track the block hash and header Info
 func (app *OperationApplication) BeginBlock(params types.RequestBeginBlock) types.ResponseBeginBlock {
-	fmt.Print("\n Inside Begin Block function \n")
+	fmt.Print("\n\n Inside Begin Block function \n")
+	fmt.Print("\n Header height: ", params.Header.Height)
+
 	return types.ResponseBeginBlock{}
 
 }
@@ -138,6 +159,14 @@ func (app *OperationApplication) DeliverTx(tx []byte) types.ResponseDeliverTx {
 	fmt.Print("\n Inside DeliverTx Module and value of tx is: ", tx)
 	time.Sleep(5)
 	fmt.Print("\n  Length of tx is: ", len(tx))
+
+	// checking if the tx is related to validators or add/Sub Operation type txn
+	if isValidator(tx) {
+		// update the validator set in the Db
+		fmt.Println("\n Inside isvalidator if condition")
+		//app.State.TotTxn++
+		return app.execvalidatorTx(tx)
+	}
 
 	// convert from ascii to respective values
 	txStr := string(tx)
@@ -197,13 +226,13 @@ func (app *OperationApplication) DeliverTx(tx []byte) types.ResponseDeliverTx {
 	fmt.Println("\n Val of TxnTime is: ", TxnTime)
 
 	// increasing the txn Count
-	app.state.TotTxn++
-	fmt.Print("\n value of state TotTxn is: ", app.state.TotTxn)
+	app.State.TotTxn++
+	fmt.Print("\n value of state TotTxn is: ", app.State.TotTxn)
 
 	return types.ResponseDeliverTx{
 		Code: code.CodeTypeOK,
 		Data: dataHash,
-		Log:  fmt.Sprintf("Txn executed with timestamp : %s and TxnCount is %d", TxnTime, app.state.TotTxn)}
+		Log:  fmt.Sprintf("Txn executed with timestamp : %s and TxnCount is %d", TxnTime, app.State.TotTxn)}
 }
 
 // CheckTx method
@@ -213,21 +242,21 @@ func (app *OperationApplication) CheckTx(tx []byte) types.ResponseCheckTx {
 
 	// convert from ascii to respective values
 	txStr := string(tx)
-	fmt.Print("\n Inside the Check_tx module nd value of tx is: ", txStr)
+	fmt.Print("\n\n Inside the Check_tx module nd value of tx is: ", txStr)
 
 	// checking if the data is empty. If yes then return badCode
-	fmt.Print("\n value of txnCount is: ", app.state.TotTxn)
-	if app.state.TotTxn == 0 {
+	fmt.Print("\n value of txnCount is: ", app.State.TotTxn)
+	if app.State.TotTxn == 0 && app.State.InfoState.LastBlockHeight != 0 {
 		return types.ResponseCheckTx{
 			Code: code.CodeTypeUnknownError,
-			Log:  fmt.Sprintf("No data found and TxnCount is %d ", app.state.TotTxn)}
+			Log:  fmt.Sprintf("No data found and TxnCount is %d ", app.State.TotTxn)}
 
 	}
 	return types.ResponseCheckTx{
 		Code: code.CodeTypeOK,
 		//		Data: dataHash,
 		//		Log:  fmt.Sprintf("Txn is not empty.Txn stored at time: %s", storeresult.Timestamp)
-		Log: fmt.Sprintf("Txn is not empty.Txn count is: %d", app.state.TotTxn)}
+		Log: fmt.Sprintf("Txn count is: %d", app.State.TotTxn)}
 
 	//return types.ResponseCheckTx{Code: code.CodeTypeOK}
 }
@@ -235,25 +264,25 @@ func (app *OperationApplication) CheckTx(tx []byte) types.ResponseCheckTx {
 // Commit method
 func (app *OperationApplication) Commit() types.ResponseCommit {
 
-	fmt.Print("\n\n Inside Commmit method and txnCount is: ", app.state.TotTxn)
+	fmt.Print("\n\n Inside Commmit method and txnCount is: ", app.State.TotTxn)
 
-	if app.state.TotTxn == 0 {
+	if app.State.TotTxn == 0 {
 		return types.ResponseCommit{}
 	}
 	appHash := make([]byte, 8)
-	binary.PutUvarint(appHash, uint64(app.state.TotTxn))
+	binary.PutUvarint(appHash, uint64(app.State.TotTxn))
 
-	app.state.AppHash = appHash
+	app.State.AppHash = appHash
 	// this is the one of the those hash which is compared to ResponseInfo LastBlockHash when we starting
 	// tendermint node. If this hash is not as same as resinfo hash then we won't be able to start
 	// the node. will get Replay error which is under checkAppHash function in replay.go
-	app.state.state.AppHash = appHash
+	app.State.InfoState.AppHash = appHash
 
-	fmt.Printf("\n Value of app.state.state.AppHash in HEX is: %X", app.state.state.AppHash)
-	app.state.Height++
-	app.state.state.LastBlockHeight = app.state.Height
-	fmt.Printf("\n Value of app.state.state.LastBlockHeight is: %d", app.state.state.LastBlockHeight)
-	saveState(app.state)
+	fmt.Printf("\n Value of app.state.state.AppHash in HEX is: %X", app.State.InfoState.AppHash)
+	app.State.Height++
+	app.State.InfoState.LastBlockHeight = app.State.Height
+	fmt.Printf("\n Value of app.state.state.LastBlockHeight is: %d", app.State.InfoState.LastBlockHeight)
+	saveState(app.State)
 	return types.ResponseCommit{Data: appHash}
 }
 
@@ -271,7 +300,7 @@ func (app *OperationApplication) Query(reqQuery types.RequestQuery) (resQuery ty
 	case "sub":
 		docsCount, err = db.C("sub").Count()
 	case "txn":
-		docsCount = int(app.state.TotTxn)
+		docsCount = int(app.State.TotTxn)
 		//case "hash":
 		//	docsCount = string(app.state.AppHash)
 	}
@@ -284,4 +313,124 @@ func (app *OperationApplication) Query(reqQuery types.RequestQuery) (resQuery ty
 	fmt.Printf("\n Total number of transaction of operation %s is %d  ", queryRequest, appHash)
 	//fmt.Println("\n after formatting: ", []byte(fmt.Sprintf("%02x", docsCount)))
 	return types.ResponseQuery{Value: appHash}
+}
+
+// InitChain function
+func (app *OperationApplication) InitChain(req types.RequestInitChain) types.ResponseInitChain {
+	fmt.Print("\n\n Inside InitChain function ")
+	for _, v := range req.Validators {
+		result := app.updateValidator(v)
+		if result.IsErr() { // IsErr() return true if code is not OK
+			panic(result)
+		}
+	}
+	return types.ResponseInitChain{}
+}
+
+// EndBlock function
+func (app *OperationApplication) EndBlock(req types.RequestEndBlock) types.ResponseEndBlock {
+	fmt.Print("\n\n Inside EndBlock Method")
+	return types.ResponseEndBlock{ValidatorUpdates: app.ValUpdates}
+}
+
+// UpdateValidator function to check the validity of validator and to insert the validator data to Db
+func (app *OperationApplication) updateValidator(v types.ValidatorUpdate) (res types.ResponseDeliverTx) {
+	fmt.Print("\n\n Inside Update Validator method")
+	//key := []byte("val:" + string(v.PubKey.Data))
+	// decoding from hex to bytes
+	key, decodeErr := hex.DecodeString(fmt.Sprintf("%x", v.PubKey.Data))
+	if decodeErr != nil {
+		panic(decodeErr)
+	}
+
+	//fmt.Print("\n Value of Key is: ", key)
+	//fmt.Print("\n Value of decodeFromHex is: ", decodefromHex)
+	fmt.Println("\n Value of v.Power is: ", v.Power)
+
+	pubkey := b64.StdEncoding.EncodeToString(key)
+	fmt.Print("\n Value of pubkey after encoding tostring is; ", pubkey)
+
+	if v.Power == 0 {
+		//remove validator
+		errMongo := db.C("ValidatorList").Find(key)
+		if errMongo != nil {
+			return types.ResponseDeliverTx{
+				Code: code.CodeTypeUnauthorized,
+				Log:  fmt.Sprintf("Canot remove non-existent validator %X ", key),
+			}
+		}
+		// remove the entry if key is found with zero Power
+		err := db.C("ValidatorList").Remove(key)
+		if err != nil {
+			panic(err)
+		}
+
+	} else {
+		// add or update validator
+		validator := MyValidator{
+			Name:   "",
+			Power:  v.Power,
+			PubKey: key,
+		}
+		fmt.Print("\nVal of validator.Pubkey is: ", validator.PubKey)
+
+		fmt.Print("\n Value of Validator struct is: ", validator)
+		errMongo := db.C("ValidatorList").Insert(validator)
+		if errMongo != nil {
+			panic(errMongo)
+		}
+		app.ValUpdates = append(app.ValUpdates, v)
+		fmt.Print("\n\n Value of ValUpddates is: ", app.ValUpdates)
+	}
+	// we only update the changes array if we successfully updated the tree
+	app.State.TotTxn++
+	return types.ResponseDeliverTx{Code: code.CodeTypeOK}
+}
+
+// execvalidatorTx method
+// format is "val:pubkey,power"
+// pubkey is raw 32-byte ed25519 key
+func (app *OperationApplication) execvalidatorTx(tx []byte) types.ResponseDeliverTx {
+	tx = tx[len(ValidatorSetChangePrefix):]
+	fmt.Print("\n\n Inside execvalidator \n value of tx is: ", string(tx))
+	//fmt.Print("\n Value of ed25519.PubKeySize is: ", ed25519.PubKeyEd25519Size, "\n")
+	// get the pubkey and power
+	pubKeyAndPower := strings.Split(string(tx), ",")
+	//fmt.Print("\n Value of pubkey adn power after split is: ", len(pubKeyAndPower))
+	if len(pubKeyAndPower) != 2 {
+		return types.ResponseDeliverTx{
+			Code: code.CodeTypeEncodingError,
+			Log:  fmt.Sprintf("Expected 'pubkey/power'. Got %v ", pubKeyAndPower),
+		}
+	}
+	pubKeyS, powerS := pubKeyAndPower[0], pubKeyAndPower[1]
+	//fmt.Print("\n Value of pubkeyS is: ", pubKeyS, "\n powerS is: ", powerS)
+
+	// decode the pubKey
+	pubkey, errPub := hex.DecodeString(pubKeyS)
+	//fmt.Print("\n value of pubkey is: ", string(pubkey))
+	if errPub != nil {
+		return types.ResponseDeliverTx{
+			Code: code.CodeTypeEncodingError,
+			Log:  fmt.Sprintf("PubKey (%s) is invalid hex", pubKeyS),
+		}
+	}
+
+	// decode the power
+	power, errPow := strconv.ParseInt(powerS, 10, 64)
+	if errPow != nil {
+		return types.ResponseDeliverTx{
+			Code: code.CodeTypeEncodingError,
+			Log:  fmt.Sprintf("Power (%s) is not an Int", powerS),
+		}
+	}
+
+	//app.State.TotTxn++
+	//update
+	return app.updateValidator(types.Ed25519ValidatorUpdate(pubkey, int64(power)))
+}
+
+// method to check whether the tx has "val:" as a prefix
+func isValidator(tx []byte) bool {
+	return strings.HasPrefix(string(tx), ValidatorSetChangePrefix)
 }
